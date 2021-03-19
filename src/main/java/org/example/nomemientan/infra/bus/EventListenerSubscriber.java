148ba417ce42271f9056154bc9dc834b0b3a6902
Raul A. Alzate <alzategomez.raul@gmail.com>
2021-03-19 10:02:06 -0500

    ADD queue configure

--- src/main/java/org/example/nomemientan/infra/bus/EventListenerSubscriber.java
+++ src/main/java/org/example/nomemientan/infra/bus/EventListenerSubscriber.java
@@ -1,5 +1,6 @@
 package org.example.nomemientan.infra.bus;
 
+import co.com.sofka.application.ApplicationEventDrive;
 import co.com.sofka.business.generic.UseCase;
 import co.com.sofka.business.generic.UseCaseHandler;
 import co.com.sofka.business.repository.DomainEventRepository;
@@ -7,13 +8,13 @@ import co.com.sofka.business.support.ResponseEvents;
 import co.com.sofka.business.support.TriggeredEvent;
 import co.com.sofka.domain.generic.DomainEvent;
 import co.com.sofka.infraestructure.asyn.SubscriberEvent;
+import co.com.sofka.infraestructure.event.EventSerializer;
 import co.com.sofka.infraestructure.repository.EventStoreRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Component;
 
-import java.util.List;
-import java.util.Objects;
-import java.util.Set;
+
 import java.util.concurrent.Flow;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -21,51 +22,35 @@ import java.util.logging.Logger;
 @Component
 public class EventListenerSubscriber implements Flow.Subscriber<DomainEvent> {
     private static final Logger logger = Logger.getLogger(EventListenerSubscriber.class.getName());
-    private final Set<UseCase.UseCaseWrap> useCases;
-    private final EventStoreRepository repository;
-    private final SubscriberEvent subscriberEvent;
-
 
     @Autowired
-    public EventListenerSubscriber(Set<UseCase.UseCaseWrap> useCases, EventStoreRepository repository, SubscriberEvent subscriberEvent) {
-        this.useCases = useCases;
-        this.repository = repository;
-        this.subscriberEvent = subscriberEvent;
-    }
+    private ApplicationEventPublisher applicationEventPublisher;
+
+    @Autowired
+    private ApplicationEventDrive applicationEventDrive;
+    private Flow.Subscription subscription;
 
     @Override
     public void onSubscribe(Flow.Subscription subscription) {
-        subscription.request(100);
+        subscription.request(1);
+        this.subscription = subscription;
     }
 
     @Override
-    public void onNext(DomainEvent domainEvent) {
-        DomainEvent event = Objects.requireNonNull(domainEvent);
-        this.useCases.stream().filter((useCaseWrap) -> useCaseWrap.eventType().equals(domainEvent.type)).forEach((useCaseWrap) -> {
-            UseCase<TriggeredEvent<? extends DomainEvent>, ResponseEvents> useCase = useCaseWrap.usecase();
-            useCase.addRepository(new DomainEventRepository() {
-                public List<DomainEvent> getEventsBy(String aggregateRootId) {
-                    return EventListenerSubscriber.this.repository.getEventsBy(event.getAggregateName(), aggregateRootId);
-                }
-
-                public List<DomainEvent> getEventsBy(String aggregate, String aggregateRootId) {
-                    return EventListenerSubscriber.this.repository.getEventsBy(aggregate, aggregateRootId);
-                }
-            });
-            UseCaseHandler.getInstance()
-                    .asyncExecutor(useCase, new TriggeredEvent<>(event))
-                    .subscribe(this.subscriberEvent);
-        });
+    public void onNext(DomainEvent event) {
+        logger.log(Level.INFO, "Process event {0}", EventSerializer.instance().serialize(event));
+        applicationEventPublisher.publishEvent(event);
+        applicationEventDrive.fire(event);
+        subscription.request(1);
     }
 
     @Override
     public void onError(Throwable throwable) {
-        logger.log(Level.SEVERE, throwable.getMessage(), throwable);
+        logger.log(Level.SEVERE, throwable.getMessage());
     }
 
     @Override
     public void onComplete() {
-
     }
 
 }
---
+++ src/main/java/org/example/nomemientan/infra/handle/JuegoMaterialize.java
@@ -0,0 +1,57 @@
+package org.example.nomemientan.infra.handle;
+
+import co.com.sofka.domain.generic.DomainEvent;
+import org.example.nomemientan.domain.juego.events.JuegoCreado;
+import org.example.nomemientan.domain.juego.events.JugadorAdicionado;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.event.EventListener;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.data.mongodb.core.query.Update;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Component;
+
+import java.util.*;
+import java.util.logging.Logger;
+
+@Component
+public class JuegoMaterialize {
+
+    private static final String COLLECTION_NAME = "juegos";
+    private static final Logger logger = Logger.getLogger(JuegoMaterialize.class.getName());
+
+    @Autowired
+    private MongoTemplate mongoTemplate;
+
+    @Async
+    @EventListener
+    public void handleEventJuegoCreado(JuegoCreado juegoCreado) {
+        logger.info("****** Handle event juegoCreado");
+        Map<String, Object> data = new HashMap<>();
+        data.put("_id", juegoCreado.getJuegoId().value());
+        mongoTemplate.save(data, COLLECTION_NAME);
+    }
+
+    @Async
+    @EventListener
+    public void handleEventJuegoCreado(JugadorAdicionado jugadorAdicionado) {
+        logger.info("****** Handle event jugadorAdicionado");
+        Update update = new Update();
+        var id = jugadorAdicionado.getJugadorId().value();
+        update.set("jogadores."+id+".capital", jugadorAdicionado.getCapital().value());
+        update.set("jogadores."+id+".capital", jugadorAdicionado.getNombre().value());
+
+        mongoTemplate.updateFirst(getFilterByAggregateId(jugadorAdicionado), update, COLLECTION_NAME);
+    }
+
+
+    private Query getFilterByAggregateId(DomainEvent event) {
+        return new Query(
+                Criteria.where("_id").is(event.aggregateRootId())
+        );
+    }
+
+
+
+}
